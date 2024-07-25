bfe29aa69573a461086b7bd20b4d3e4a2c041c3c
jovangolic <118064547+jovangolic@users.noreply.github.com>
2024-07-25 14:12:06 +0200

    Update About.jsx

--- cinema-front/src/components/about/About.jsx
+++ cinema-front/src/components/about/About.jsx
@@ -4,13 +4,12 @@ const About = () => {
 
     return(
         <section className="mt-5 mb-5 container">
-            <div className="d-flex justify-content-left">
-                <h3 className="justify-content-center">About</h3>
-                <div className="d-flex justify-content-center">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequuntur error,
+            <div className="text-center">
+                <h3>About</h3>
+                <hr/>
+                <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequuntur error,
                     dolorum numquam culpa explicabo molestias accusamus inventore consequatur iste iure! 
                     Laborum distinctio ullam temporibus aspernatur eum expedita veniam architecto?</p>
-                </div>
             </div>
         </section>
     );
