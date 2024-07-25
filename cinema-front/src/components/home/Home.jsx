44d75805475b511a85ea688747d6af3157d23ffd
jovangolic <118064547+jovangolic@users.noreply.github.com>
2024-07-25 14:13:27 +0200

    Update Home.jsx

--- cinema-front/src/components/home/Home.jsx
+++ cinema-front/src/components/home/Home.jsx
@@ -1,29 +1,34 @@
-import React from "react"
-import { useLocation } from "react-router-dom";
-import MainHeader from "../layout/MainHeader";
-import Parallax from "../common/Parallax";
-import CinemaService from "../common/CinemaService";
-
+import React, { useContext } from "react"
+import MainHeader from "../layout/MainHeader"
+import HotelService from "../common/HotelService"
+import Parallax from "../common/Parallax"
+import RoomCarousel from "../common/RoomCarousel"
+import RoomSearch from "../common/RoomSearch"
+import { useLocation } from "react-router-dom"
+import { useAuth } from "../auth/AuthProvider"
 const Home = () => {
-    const location = useLocation()
+	const location = useLocation()
 
 	const message = location.state && location.state.message
 	const currentUser = localStorage.getItem("userId")
-
-    return(
-        <section>
-            {message && <p className="text-warning px-5">{message}</p>}
+	return (
+		<section>
+			{message && <p className="text-warning px-5">{message}</p>}
 			{currentUser && (
 				<h6 className="text-success text-center"> You are logged-In as {currentUser}</h6>
 			)}
-            <MainHeader />
-            <section className="container">
-                <Parallax />
-                <CinemaService />
-                <Parallax />
-            </section>
-        </section>
-    );
-};
+			<MainHeader />
+			<div className="container">
+				<RoomSearch />
+				<RoomCarousel />
+				<Parallax />
+				<RoomCarousel />
+				<HotelService />
+				<Parallax />
+				<RoomCarousel />
+			</div>
+		</section>
+	)
+}
 
-export default Home;
+export default Home
